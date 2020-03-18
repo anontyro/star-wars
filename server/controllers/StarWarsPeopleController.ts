79cab4a20c023bab1f00525a5fcfee91bc2a1aff
alexWilkinson <alex@hooq.tv>
2020-03-18 16:00:00 +0800

    temp moved types here to resolve error finding them, and added in id and image

--- server/controllers/StarWarsPeopleController.ts
+++ server/controllers/StarWarsPeopleController.ts
@@ -5,6 +5,41 @@ import { Request, Response } from 'express';
 import fetch from 'node-fetch';
 import { API_ROOT, API_PEOPLE } from '../consts';
 
+interface PeopleResponse {
+  count: number;
+  next: string;
+  previous: number;
+  results: Person[];
+}
+
+interface Person {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: Date;
+  edited: Date;
+  url: string;
+  image_url?: string;
+  id?: number;
+}
+
+interface PeopleState {
+  peopleList: Person[];
+  currentPerson: Person | null;
+  isBusy: boolean;
+}
+
+const DEFAULT_IMAGE_ROOT = '/static/images/main_logo.png';
 const PEOPLE_IMAGE_ROOT = '/static/images/people/';
 
 const peopleImages = {
@@ -22,16 +57,38 @@ let peopleCache = {};
 
 let personCache = {};
 
+const getId = (page: number, pageSize: number = 10) => (index: number): number => {
+  const correctedIndex = index + 1;
+  if (page === 1) {
+    return correctedIndex;
+  }
+  return (page - 1) * pageSize + correctedIndex;
+};
+
+const setItemId = (people: Person[], page: number): Person[] =>
+  people.map((person: Person, index: number) => {
+    const correctedId = getId(page)(index);
+    return {
+      ...person,
+      id: correctedId,
+      image_url: peopleImages[correctedId] ? peopleImages[correctedId].image : DEFAULT_IMAGE_ROOT,
+    };
+  });
+
 const getPeopleFromCache = async (page: number) => {
   if (peopleCache[page]) {
     return peopleCache[page];
   }
   const url = `${API_ROOT}${API_PEOPLE}?page=${page}`;
   const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
-  peopleCache[page] = json;
-  return json;
+  const json: PeopleResponse = await response.json();
+  const peopleList: PeopleResponse = {
+    ...json,
+    results: [...setItemId(json.results, page)],
+  };
+  console.log(peopleList);
+  peopleCache[page] = peopleList;
+  return peopleCache[page];
 };
 
 const getPersonFromCache = async id => {
